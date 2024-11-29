Got Minimizer working. Needed to fix e_0 and k_0 to scale with change in units

Integrator.py
@@ -1,11 +1,6 @@
 import numpy as np
 import scipy as sp;
 from Utilities import *
-import copy
-
-#           We can make this more efficient: there doesn't need to be different functions for each. Also, in 
-#ODESolver, we're running RK4 x5 times each iteration. See bottom of page for alternate
-
 
 # Derivatives of the dependent variables.
 # Notably, density is absent. Use Utilities.equation_of_state to generate the density.
@@ -29,8 +24,12 @@ def L_prime_der(current, extra_const_params):
     return output
 
 def T_prime_der(current, extra_const_params):
-    new_T = - extra_const_params["kappa_prime"]* current[DENSITY_UNIT_INDEX] * current[LUMINOSITY_UNIT_INDEX] * np.power(current[RADIUS_UNIT_INDEX],-4) * np.power(current[TEMP_UNIT_INDEX],-6.5)
+    # assume that kappa_prime is in args
     output = np.zeros(current.shape)
+    multiplied_vars = current[TEMP_UNIT_INDEX]* current[RADIUS_UNIT_INDEX]
+    var = np.power(multiplied_vars, -4)
+    tp = np.power(current[TEMP_UNIT_INDEX],-2.5)
+    new_T = - extra_const_params["kappa_prime"]* current[DENSITY_UNIT_INDEX] * current[LUMINOSITY_UNIT_INDEX] * var * tp 
     output[TEMP_UNIT_INDEX] = new_T
     return output
 
@@ -92,64 +91,21 @@ def ODESolver(initial_conditions, num_steps, extra_const_parameters):
     derivatives = [r_prime_der, P_prime_der, L_prime_der, T_prime_der] #array of differential equations.
     state = np.zeros((6,num_steps)) #initializing array of variables.
     state[:,0] = initial_conditions
+#    print(initial_conditions[TEMP_UNIT_INDEX], initial_conditions[PRESSURE_UNIT_INDEX])
+#    print(initial_conditions)
     for i in range(1, num_steps):
         #RK4 receives a specific differential equation corresponding to each variable of interest. 
         #RK4 outputs a 6x1 array with elements of: mass, radius, pressure, luminosity, 
         #temperature, and density. Only the element corresponding to the differential equation (derivatives[n])
         #input into RK4 has the correct updated value.
-        state[MASS_UNIT_INDEX,i] = state[MASS_UNIT_INDEX,i-1] + step_size #Mass.
+        state[MASS_UNIT_INDEX,i] = state[MASS_UNIT_INDEX,i-1] + step_size
         state[RADIUS_UNIT_INDEX,i] = RK4(derivatives[0], state[:,i-1], step_size,extra_const_parameters)[RADIUS_UNIT_INDEX]
         state[PRESSURE_UNIT_INDEX,i] = RK4(derivatives[1], state[:,i-1], step_size,extra_const_parameters)[PRESSURE_UNIT_INDEX]
         state[LUMINOSITY_UNIT_INDEX,i] = RK4(derivatives[2], state[:,i-1], step_size,extra_const_parameters)[LUMINOSITY_UNIT_INDEX]
         state[TEMP_UNIT_INDEX,i] = RK4(derivatives[3], state[:,i-1], step_size,extra_const_parameters)[TEMP_UNIT_INDEX]
         state[DENSITY_UNIT_INDEX,i] = equation_of_state(state[PRESSURE_UNIT_INDEX,i], state[TEMP_UNIT_INDEX,i], extra_const_parameters)
-        print(state[:,i])
 
     return state
-#------------------------------------------------------------------------------------------------------------
-
-
 
 if __name__ == "__main__":
     pass
-
-
-
-def derivatives(current, extra_const_params):
-    r_prime = (1/(4*np.pi*np.power(current[RADIUS_UNIT_INDEX],2)*current[DENSITY_UNIT_INDEX]))
-    P_prime = (-1/(4*np.pi))* (current[MASS_UNIT_INDEX])/(np.power(current[RADIUS_UNIT_INDEX], 4) )
-    L_prime = extra_const_params["E_prime"]* current[DENSITY_UNIT_INDEX]*np.power(current[TEMP_UNIT_INDEX],4)
-    T_prime = - extra_const_params["kappa_prime"]* current[DENSITY_UNIT_INDEX] * current[LUMINOSITY_UNIT_INDEX] * np.power(current[RADIUS_UNIT_INDEX],-4) * np.power(current[TEMP_UNIT_INDEX],-6.5)
-    return np.array([r_prime, P_prime, L_prime, T_prime])
-
-
-
-def RK4_Solver(initial_conditions, current, extra_const_params, step_size, step_num):
-    """
-    Inputs:
-        * initial_conditions: first entry of state_array
-        * current: encodes the current state of the system at mass m as a 6x1 np array
-                0th term is independent variable, and the rest are the dependent ones
-        * extra_const_params: see Utilities.py
-        * step_size: how big of a step in x do you want
-        * step_num: number of total steps
-    Outputs:
-        * state_array: all parameters at each mass step.
-    """
-    assert(step_size >0)
-
-    state_array = np.zeros((6,step_num)) #initializing array of variables.
-    state_array[:,0] = initial_conditions
-
-    for m in range(1, step_num):
-        state_array = np.append(state_array, current)
-        k1 = step_size*derivatives(current, extra_const_params)
-        k2 = step_size*derivatives(current + 0.5*k1, extra_const_params)
-        k3 = step_size*derivatives(current + 0.5*k2, extra_const_params)
-        k4 = step_size*(current + k3, extra_const_params)
-        
-        current[RADIUS_UNIT_INDEX,PRESSURE_UNIT_INDEX,LUMINOSITY_UNIT_INDEX,TEMP_UNIT_INDEX] += (k1 + 2*k2 + 2*k3 + k4)/6
-        current[MASS_UNIT_INDEX] = current[MASS_UNIT_INDEX] + step_size
-        current[DENSITY_UNIT_INDEX] = equation_of_state(current[PRESSURE_UNIT_INDEX], current[TEMP_UNIT_INDEX], extra_const_params)
-    
-    return state_array

IntegratorTest.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 import Integrator
 
-def derivative_func(state):
+def derivative_func(state, extra):
     return -state
 
 
@@ -12,7 +12,7 @@ class TestMinimizer:
         """
             Use the IVP of dy/dt = -y. y(0) = 1.
             This should have the solution y(t) = e(-t)
-            We make 1000 steps, and then check against the analytic result
+            We make 1000 steps, and then check against the analytic result at each step
         """
         # Extraneous 0 after 1 because actual problem has 5 dependent variables
         state = np.array([0,1 ])
@@ -27,7 +27,7 @@ class TestMinimizer:
             actual_val = np.exp(-x[i])
             error = np.abs(actual_val-state[1])
             assert (error < tolerance)
-            state = Integrator.RK4(derivative_func, state, step_size)
+            state = Integrator.RK4(derivative_func, state, step_size,{})
     def test_ODESolver(self):
         pass
 

Minimizer.py
@@ -10,10 +10,10 @@ def gen_initial_conditions(starting_scaled_temp, starting_scaled_pressure, step_
     initial_mass = step_size/2
     initial_rad = np.power((4*np.pi/3)*initial_mass/initial_density, 1/3)
 #    initial_lum = const_params["E_prime"]*initial_density*np.power(starting_scaled_temp,4)* initial_mass
-    new_pressure = starting_scaled_pressure-(3/8/np.pi)*np.power((4*np.pi*initial_density/3),4/3)*np.power(initial_mass,2/3)
-    new_temp = new_pressure*const_params["mu"]/initial_density
+#    new_pressure = starting_scaled_pressure-(3/8/np.pi)*np.power((4*np.pi*initial_density/3),4/3)*np.power(initial_mass,2/3)
+#    new_temp = new_pressure*const_params["mu"]/initial_density
     initial_conds = np.array(  (initial_mass,initial_rad,initial_density,
-                                new_pressure , 0, new_temp) )
+                                starting_scaled_pressure , 0, starting_scaled_temp) )
     return initial_conds
 
 def loss_function(estimator_guess, *args):
@@ -28,8 +28,8 @@ def loss_function(estimator_guess, *args):
 # Temperature and pressure normally can't be negative
     initial_pressure  = estimator_guess[0]
     initial_temp  = estimator_guess[1]
-    assert(initial_pressure >= 0) 
-    assert(initial_temp >= 0)
+#    assert(initial_pressure >= 0) 
+#    assert(initial_temp >= 0)
 # Want temperature and pressure to be 0 at boundaries. These variables are mostly just for clarity
     expected_pressure = np.float64(0)
     expected_temp = np.float64(0)
@@ -48,7 +48,12 @@ def run_minimizer(Initial_scaled_T, Initial_scaled_P, num_iters, M_0, R_0, epsil
     x0 = np.array([Initial_scaled_T, Initial_scaled_P])
     solver = Integrator.ODESolver
     extra_const_params = Utilities.generate_extra_parameters(M_0, R_0, epsilon, kappa, mu)
-    return sp.optimize.minimize(loss_function,x0, args=(solver,num_iters,extra_const_params))
+    return sp.optimize.minimize(loss_function,x0,
+                                args=(solver,num_iters,extra_const_params),
+                                bounds=sp.optimize.Bounds(
+                                    lb=[Utilities.global_tolerance,Utilities.global_tolerance],
+                                    ub=[np.inf,np.inf], keep_feasible=[True,True]),
+                                )
 
 if __name__ == "__main__":
     pass

MinimizerTest.py
@@ -2,12 +2,26 @@
 import numpy as np
 import pytest
 import Minimizer
+import Utilities
+import Integrator
 
 class TestMinimizer:
     def test_minimizer(self):
-    # Make sure that minimmizer can be run from end to end
-        ns = 1000
-        out  = Minimizer.run_minimizer(10,10,ns,1,2,3,4,5)
-
+        # Make sure that When using Sun parameters, you get core temps on the same order as the accepted values
+        length_scale = Utilities.R_sun
+        mass_scale = Utilities.M_sun
+        mu = Utilities.mu_sun
+        scale_factors = Utilities.UnitScalingFactors(mass_scale, length_scale)
+        params = Utilities.generate_extra_parameters(mass_scale, length_scale,Utilities.E_0_sun, Utilities.kappa_0_sun,Utilities.mu_sun)
+        results = Minimizer.run_minimizer(2E7/scale_factors[Utilities.TEMP_UNIT_INDEX],30E15/scale_factors[Utilities.PRESSURE_UNIT_INDEX],
+                                        1000, mass_scale, length_scale, params["E_prime"], params["kappa_prime"], mu)
+        estimated_core_pressure = results.x[1]*scale_factors[Utilities.PRESSURE_UNIT_INDEX]
+        estimated_core_temp = results.x[0]*scale_factors[Utilities.TEMP_UNIT_INDEX]
+        expected_core_temp = 1.5E7 # K
+        expected_core_pressure = 26.5E6*1E9 # Pa
+# This one is off by a factor of 2 for some reason
+        assert(np.floor(np.log10(estimated_core_temp))==np.floor(np.log10(expected_core_temp)))
+# This one is off by a factor of 3 for some reason
+        assert(np.floor(np.log10(estimated_core_pressure))==np.floor(np.log10(expected_core_pressure)))
 if __name__ == "__main__":
     pass

Plotting.py
@@ -1,7 +1,26 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import Integrator
+import Minimizer
 from Utilities import *
+
+def plot_variable(independent, dependent, title, filename, xlabel, ylabel, logx = False, logy = False, clear= True):
+    """
+    Helper function to plot variables in the mesh against each other
+    """
+    if (clear):
+        plt.clf()
+    plt.figure(figsize=(20, 10)) 
+    plt.title(title)
+    plt.xlabel(xlabel) 
+    plt.ylabel(ylabel)
+    if logx:
+        plt.xscale("log")
+    if logy:
+        plt.yscale("log")
+#    plt.grid(True)
+    plt.plot(independent, dependent)
+    plt.savefig(filename+'.png')
 
 if __name__ == "__main__":  # Main guard ensures this code runs only when the script is executed directly
     pass
@@ -15,33 +34,26 @@ if __name__ == "__main__":  # Main guard ensures this code runs only when the sc
 #TIME_UNIT_INDEX = 6 
 
 # Generate multi-star states with 3 sets of initial conditions. INPUT INITIAL CONDITIONS.
-state0 = Integrator.ODESolver([0,0,0,2.5E14,0,1.5E7], 100, generate_extra_parameters(M_sun, R_sun, 1.4E5, 0.2, mu_sun))  
-state1 = Integrator.ODESolver([0,0,0,0,0,0], 100, generate_extra_parameters()) 
-state2 = Integrator.ODESolver([0,0,0,0,0,0], 100, generate_extra_parameters()) 
-
-MSS = np.stack([state0, state1, state2], axis=2) #7x(step-size)x3 array. MSS = Multi-Star-States
+conversion = UnitScalingFactors(M_sun, R_sun)
+constants = generate_extra_parameters(M_sun, R_sun, E_0_sun, kappa_0_sun, mu_sun)
+print(constants)
+init_conds = Minimizer.gen_initial_conditions(1.5E7/conversion[TEMP_UNIT_INDEX], 26.5E6*1E9/conversion[PRESSURE_UNIT_INDEX],1E-2, constants)
+print(init_conds)
+state0 = Integrator.ODESolver(init_conds, 100, constants)
     
-radius = MSS[RADIUS_UNIT_INDEX,:,:] #Radius
+radius = state0[RADIUS_UNIT_INDEX,:] #Radius
 #Extracting variables to be plotted over all 3 initial conditions and all mass steps.
 variables = [
-MSS[DENSITY_UNIT_INDEX,:,:], #Density
-MSS[TEMP_UNIT_INDEX,:,:], #Temperature
-MSS[PRESSURE_UNIT_INDEX,:,:], #Pressure
-MSS[LUMINOSITY_UNIT_INDEX,:,:], #Luminosity
+state0[DENSITY_UNIT_INDEX,:], #Density
+state0[TEMP_UNIT_INDEX,:], #Temperature
+state0[PRESSURE_UNIT_INDEX,:], #Pressure
+state0[LUMINOSITY_UNIT_INDEX,:], #Luminosity
             ]
 labels = ['Density', 'Temperature', 'Pressure', 'Luminosity']
 units = ['g/cm³', 'K', 'Pa', 'W'] #Replace with actual units
-print(np.shape(variables))
-#Plot dimensionless dependent variable vs. radius:
-plt.figure(figsize=(20, 10)) 
-plt.title('Stellar Radial Dependency of Density')
-plt.xlabel('Radius') 
-plt.ylabel('Density')
-plt.grid(True)
-#for i in range(1,3):
-    #plt.plot(radius[:, i], variables[:, i], label=f'Initial Condition {i}') #Each variable is a 1xNx3 array, i.e. a 2D Nx3 array, so we iterate over each initial condition.
-#plt.legend()
-#plt.savefig('Density_R.png')
+plot_variable(radius, variables[0],'Stellar Radial Dependency of Density',"Density_R", 'Radius', 'Density', logy =True )
+plot_variable(radius, variables[1],'Stellar Radial Dependency of Temperature',"Temp_R", 'Radius', 'Temp', logy =True )
+plot_variable(radius, variables[2],'Stellar Radial Dependency of Pressure',"Pres_R", 'Radius', 'Pressure', logy =True )
+plot_variable(radius, variables[3],'Stellar Radial Dependency of Luminosity',"Lum_R", 'Radius', 'Luminosity', logy =True )
 
-
-#multiply array by x_out array from Utilities for last question of problem.
+print(variables[3])

Utilities.py
@@ -10,6 +10,9 @@ m_p = np.float64(1.67262192E-27) # kg
 M_sun = 1.989E30 # kg
 R_sun  = 6.9634E8 # m
 mu_sun = np.float64(0.6)
+#E_0_sun = 1.8E-39 # m^5/(kg s^3*K^4)
+E_0_sun = 1.8E-26 # m^5/(kg s^3*K^4)
+kappa_0_sun = .03 # m^2/kg
 
 # Numerical Resolution used throughout the sim
 global_tolerance = 1E-9
@@ -53,7 +56,6 @@ def UnitScalingFactors( M_0, R_0):
            t_0
         ])
 
-
 def generate_extra_parameters(M_0, R_0, E_0, kappa, mu):
     """
         Given the unitful parameters of the problem, generate the unitless constants to be used in the simulation
@@ -66,12 +68,15 @@ def generate_extra_parameters(M_0, R_0, E_0, kappa, mu):
     Output:
         extra_const_params: python dictionary containing the converted constant parameters
     """
-    scale_factors = UnitScalingFactors(M_0, R_0)
-    t_0 = np.sqrt(np.power(R_0,3) / (G*M_0))
-    T_0 = scale_factors[TEMP_UNIT_INDEX]
-    new_ep = E_0/(scale_factors[DENSITY_UNIT_INDEX]*np.power(T_0,4))*(M_0/scale_factors[LUMINOSITY_UNIT_INDEX])
-#    new_ep = E_0* np.power(t_0,3)*M_0*np.power(T_0,4)/ np.power(R_0,5) #E_0 is dependent on the main reactants (proton-proton fusion for the Sun)
-    new_kp = kappa* (3/(16*StefanBoltz)) * np.power(M_0,3)/ (np.power(R_0,5)*np.power( T_0,7.5)* np.power(t_0,3)) #kappa_0 is dependent on the region of interest and varies from 0.02 (core) to 0.001 (exterior)
+    _,_,rho0, P0,L0,T0,t0 = UnitScalingFactors(M_0, R_0)
+    ep_prefactor = np.power(R_0,5)*np.power(M_0,-1)*np.power(T0,-4)*np.power(t0,-3)
+    new_ep = E_0/ep_prefactor
+#    print("SCALED_EP: ", E_0, ep_prefactor, new_ep)
+
+    kp_prefactor = np.power(M_0,-2)*np.power(R_0,5)*np.power(T0,3.5)
+    kp_const_prefactor = (3/(16*StefanBoltz*np.power(4*np.pi,2) ))
+    new_kp = kp_const_prefactor*kappa/kp_prefactor
+#    print("SCALED_KP: ", kappa, kp_prefactor,kp_const_prefactor, new_kp)
     
     extra_const_params = {
         "mu": mu,
@@ -79,7 +84,7 @@ def generate_extra_parameters(M_0, R_0, E_0, kappa, mu):
     "kappa_prime": new_kp,
 #    "E_prime": E_0,
 #    "kappa_prime": kappa,
-    } 
+    }
 
     return extra_const_params
 