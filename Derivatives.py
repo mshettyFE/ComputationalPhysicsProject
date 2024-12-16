Fixed error in residual calculation. Still doesn't fix large changes in variables

Derivatives.py
@@ -100,14 +100,15 @@ def Jac_block_rl(state_vector_matrix_interp, dm, constants, verbose=False):
     return output
 
 # Difference equation for Pressure
-# (P_{k+1}-P_{k})/(dm) +  (dm/2)/(4*pi*r^{4}_{half})
+# (P_{k+1}-P_{k})/(dm) +  (dm_{half}/2)/(4*pi*r^{4}_{half})
 
 # Radius
 def Jac_block_pr(state_vector_matrix_interp, dm, constants, verbose=False):
     output_dim = state_vector_matrix_interp.shape[1]
     output = np.zeros((output_dim,output_dim))
     interp_rad = state_vector_matrix_interp[InterpolationIndex.RADIUS.value]
-    val = (-1/(4*np.pi)) *dm /np.power(interp_rad,5)
+    mass = state_vector_matrix_interp[InterpolationIndex.MASS.value]
+    val = (-1/(4*np.pi)) *mass /np.power(interp_rad,5)
 
     for shell in range(0,output_dim): # Run through all of the shells
         if (shell == 0): # r_0 doesn't change, so only include \delta r_{1} term

DifferenceEqs.py
@@ -1,8 +1,7 @@
 import numpy as np
 from StateVector import InterpolationIndex, StateVector, StateVectorVar
 
-# NOTE: x_{half} = 0.5(x_{k+1}-x_{k})
-# so we see that (x_{k+1}-x_{k}) = 2*x_{half}
+# dm = m_{k+1}-m_{k}
 
 # Difference equation for radius
 # (r_{k+1}-r_{k})/(dm)- 1/(4*pi*r^{2}_{half}* \rho_{half})
@@ -13,11 +12,12 @@ def calc_gr(differences, interpolation, dm):
     return rad*2/dm-1/(4*np.pi)/np.power(interp_rad,2)/density
 
 # Difference equation for Pressure
-# (P_{k+1}-P_{k})/(dm) +  (dm/2)/(4*pi*r^{4}_{half})
+# (P_{k+1}-P_{k})/(dm) +  (m_{half}/2)/(4*pi*r^{4}_{half})
 def calc_gP(differences,interpolation, dm):
     pres = differences[StateVectorVar.PRESSURE.value, :]
     rad = interpolation[InterpolationIndex.RADIUS.value,:]
-    return pres*2/dm+dm/2/(4*np.pi*np.power(rad,4))
+    masses = interpolation[InterpolationIndex.MASS.value,:]
+    return pres/dm+masses/2/(4*np.pi*np.power(rad,4))
 
 # Difference equation for Temperature
 # (T_{k+1}-T_{k})/(dm)+ \kappa_0 \rho_{half}* L_{half}/r^{4}_{half}/T^{6.5}_{half}
@@ -35,7 +35,7 @@ def calc_gL(differences, interpolation, dm, epsilon):
     temp = interpolation[InterpolationIndex.TEMP.value, :]
     density =  interpolation[InterpolationIndex.DENSITY.value,:]
     lum = differences[StateVectorVar.LUMINOSITY.value, :]
-    return lum*2/dm-epsilon*density*np.power(temp,4)    
+    return lum/dm-epsilon*density*np.power(temp,4)    
 
 def calc_g(state_vector: StateVector, parameters):
     interpolation = state_vector.interpolate_all(parameters)
@@ -44,7 +44,7 @@ def calc_g(state_vector: StateVector, parameters):
     block_size = state_vector.n_shells-1
     output = np.zeros(4*block_size)
     output[0:block_size] = calc_gr(diffs,interpolation, dm)
-    output[0:block_size] = calc_gP(diffs,interpolation, dm)
-    output[0:block_size] = calc_gT(diffs,interpolation, dm, parameters["k0_prime"])
-    output[0:block_size] = calc_gL(diffs,interpolation, dm, parameters["E0_prime"])
+    output[block_size:2*block_size] = calc_gP(diffs,interpolation, dm)
+    output[2*block_size:3*block_size] = calc_gT(diffs,interpolation, dm, parameters["k0_prime"])
+    output[3*block_size:4*block_size] = calc_gL(diffs,interpolation, dm, parameters["E0_prime"])
     return output

StateVector.py
@@ -1,20 +1,22 @@
 import numpy as np
-import sys
-from enum import Enum, auto
+from enum import Enum
 from Utilities import equation_of_state
 
+# Allows you to index self.state_vec to extract the associated variables
 class StateVectorVar(Enum):
     RADIUS= 0
     PRESSURE=1
     TEMP=2
     LUMINOSITY=3
 
+# Allows you to index into the interpolated values returned from StateVector.interpolate_all()
 class InterpolationIndex(Enum):
     RADIUS= 0
     PRESSURE=1
     TEMP=2
     LUMINOSITY=3
     DENSITY=4
+    MASS=5
 
 class StateVector():
     def __init__(self, n_shells, test_data=False):
@@ -24,9 +26,12 @@ class StateVector():
                     Assumes that state_vector has the following form:
                     <r_0, r_1,...r_{k-1}, P_0,P_1,...P_{k-1}, T_0,...T_{k-1}, L_0,...L_{k-1}>, where k is the number of shells
                 n_shells: the number of shells used
+                shell_masses: the mass value assigned to each shell
                 starting_indicies: A dictionary housing at what index does each variable begin in the state vector
         """
         self.n_shells = n_shells
+        self.dm = 1/self.n_shells
+        self.shell_masses = self.dm*np.arange(n_shells)
         if(test_data):
             output = np.zeros((4*n_shells))
             output[0:n_shells] = 2*np.arange(n_shells)
@@ -193,7 +198,8 @@ class StateVector():
         temp = self.summed_vars(StateVectorVar.TEMP)/2
         lum = self.summed_vars(StateVectorVar.LUMINOSITY)/2
         density = equation_of_state(pres, temp, constants)
-        return np.vstack([rad, pres, temp, lum, density])
+        masses = 0.5*(self.shell_masses[0:self.n_shells-1]+self.shell_masses[1:self.n_shells])
+        return np.vstack([rad, pres, temp, lum, density, masses])
 
     def diff_vars_all(self):
         """
@@ -208,9 +214,15 @@ class StateVector():
         return np.vstack([rad, pres, temp, lum])
 
     def save_state(self, filename):
+        """
+            Wrapper around np.savetxt()
+        """
         np.savetxt(filename, self.state_vec)
 
     def load_state(self, filename):
+        """
+            Wrapper around np.loadtxt()
+        """
         cand_state = np.loadtxt(filename)
         assert (cand_state.shape[0]%4 ==0)
         self.n_shells = int(cand_state.shape[0]/4)